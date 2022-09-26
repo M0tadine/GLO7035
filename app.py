6bf926ffd33e07e081c1ccd683024a711143113f
TheophileBerteloot <thber64@ulaval.ca>
2022-09-26 05:56:53 -0700

    add routes collection

--- app.py
+++ app.py
@@ -1,32 +1,21 @@
 from pymongo import MongoClient
-from initMongoDb import initRestaurantsColl
-from flask  import Flask
+from flask  import Flask,jsonify
 
-client = MongoClient('127.0.0.1',
-                     username='root',
-                     password='toor',)
-db = client['RestosMiam']
-restaurantsCol = db['restaurants']
 
-initRestaurantsColl(restaurantsCol)
+
+
+
 
 
 
 
 app = Flask(__name__)
 
-#Import the flask module
-
-#Create a Flask constructor. It takes name of the current module as the argument
-
-#Create a route decorator to tell the application, which URL should be called for the #described function and define the function
-
-@app.route('/')
-def tutorialspoint():
-    cursor = restaurantsCol.find({})
-    for document in cursor:
-          print(document)
-#Create the main driver function
-if __name__ == '__main__':
-#call the run method
-    app.run(debug=True,host='0.0.0.0')
+@app.route("/")
+def hello_world():
+    client = MongoClient('127.0.0.1',
+                     username='root',
+                     password='toor',)
+    db = client['RestosMiam']
+    restaurantsCol = db['restaurants']
+    return str(restaurantsCol.count_documents({}))
--- initMongoDb.py
+++ initMongoDb.py
@@ -7,6 +7,20 @@ from pymongo import MongoClient
 yelpAPIKey = 'Bearer LCDj57HpMzWDMxioeRYVuhuN1NlLY04Kagqtfzh_Vx09pYs-To53519ZqAWVR9PuRqxekaJgcxZa01QzGFOkXI7KgOjDhM401RtxGfu0Yv7N9n-k8UY6OONSJ54aY3Yx'
 yelpClientId = '7ymC55hnHxZyYnivO4J41Q'
 
+client = MongoClient('127.0.0.1',
+                     username='root',
+                     password='toor',)
+db = client['RestosMiam']
+
+
+routesCol = db['routes']
+routesCol.drop()
+routesCol = db['routes']
+
+restaurantsCol = db['restaurants']
+restaurantsCol.drop()
+restaurantsCol = db['restaurants']
+
 def FetchRestaurants(requestIndex) :
     res = requests.get(
         'https://api.yelp.com/v3/businesses/search?term=restaurant&location=Quebec&limit=50&radius=40000&offset=' + str(requestIndex * 50),
@@ -33,5 +47,13 @@ def initRestaurantsColl(restaurantsCol):
         if justOneFetch:
             break
 
+def initRoutesColl(routesCol):
+    f = open('./piste_cyclable.json')
+    rawData = json.load(f)
+    for data in rawData:
+        routesId = routesCol.insert_one(data).inserted_id
+    f.close()
 
 
+initRestaurantsColl(restaurantsCol)
+initRoutesColl(routesCol)
