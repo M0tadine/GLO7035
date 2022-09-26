07cc4b16cff94fe3c3f2108b4bcf1c18a38a6099
TheophileBerteloot <thber64@ulaval.ca>
2022-09-26 06:22:28 -0700

    add heartbeat endpoint

--- app.py
+++ app.py
@@ -11,11 +11,6 @@ from flask  import Flask,jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/heartbeat")
 def hello_world():
-    client = MongoClient('127.0.0.1',
-                     username='root',
-                     password='toor',)
-    db = client['RestosMiam']
-    restaurantsCol = db['restaurants']
-    return str(restaurantsCol.count_documents({}))
+    return {'villeChoisie':'Quebec'}
