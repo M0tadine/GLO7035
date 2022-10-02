b044fac60951a7bc25b0371fe6f73d77f0075315
TheophileBerteloot <thber64@ulaval.ca>
2022-10-02 09:34:31 -0700

    fix issues

--- app.py
+++ app.py
@@ -10,3 +10,7 @@ app = Flask(__name__)
 @app.route("/heartbeat")
 def hello_world():
     return {'villeChoisie':'Quebec'}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port='5000' debug=True)
